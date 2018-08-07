f220e2a5fd69a4feeb9a632f6f78adb253c28d76
novakg <novakg@VEL50DZG.auto.contiwan.com>
2018-08-07 15:13:00 +0200

    remove git stuff

--- save_all_xlib.m
+++ save_all_xlib.m
@@ -2,7 +2,6 @@
 % example: save_all_instance
 function save_all_instance
 tic;
-<<<<<<< HEAD
 fprintf('in progress...');
 save_instance('data/c15')
 fprintf('...');
@@ -22,25 +21,7 @@ toc;
 
 % zip all resulting files in output.zip for easier upload
 fprintf('zipping all files...\n');
-zip('output.zip','data');
+zip('data/output_xlib_all.zip','data');
 fprintf('done!\n');
-=======
-fprintf('in progress..');
-save_instance('data/Patterson')
-fprintf('...');
-save_instance('data/RG30/Set 1')
-save_instance('data/RG30/Set 2')
-save_instance('data/RG30/Set 3')
-save_instance('data/RG30/Set 4')
-save_instance('data/RG30/Set 5')
-fprintf('...');
-save_instance('data/RG300_corr')
-fprintf('...');
-save_instance('data/SMCP')
-fprintf('still working...');
-save_instance('data/SMFF')
-fprintf('done!\n');
-toc;
 
 % any additional dataset goes here
->>>>>>> project-c/master
