45181596ba2f775efc9f052f24249d0d623f8c85
novakge <38750734+novakge@users.noreply.github.com>
2018-08-07 19:09:16 +0200

    add support for multiple parsers

--- save_instance.m
+++ save_instance.m
@@ -1,36 +1,55 @@
 % batch processing of *.mm / *.p2x / *.rcp files to (parsed) *.mat files as an input for simulation
 % input: <folder name> containing files in the given format and extension
 % output: parsed *.mat file for each input file stored in ../<folder name>/output folder
-% example: save_instance('data_folder')
-function save_instance(directory)
+% example #1: save_instance('data_folder', 'rangen')
+% example #2: save_instance('data_folder', 'protrack')
+% example #3: save_instance('data_folder', 'xlib')
+function save_instance(directory, parser_type)
 
 % select the existing parser function and the corresponding file extension
-if exist('parse_instance') % RanGen1-2
-    parser = @parse_instance;
-    extension_filter = '*.rcp'; % select extension
-elseif exist('parse_protrack') % ProTrack (Real-Life)
-    parser = @parse_protrack;
-    extension_filter = '*.p2x'; % select extension
-elseif exist('parse_xlib') % MMLIB, PSPLIB
-    parser = @parse_xlib;
-    extension_filter = '*.mm'; % select extension
-end
 
+switch parser_type
+    
+    case 'rangen'
+        if exist('parse_instance') % RanGen1-2
+            parser = @parse_rangen; % create common function handle
+            extension_filter = '*.rcp'; % select extension
+        end
+        
+    case 'protrack'
+        if exist('parse_protrack') % ProTrack (Real-Life)
+            parser = @parse_protrack; % create common function handle
+            extension_filter = '*.p2x'; % select extension
+        end
+
+    case 'xlib'
+        if exist('parse_xlib') % MMLIB, PSPLIB
+            parser = @parse_xlib; % create common function handle
+            extension_filter = '*.mm'; % select extension
+        end
+
+    otherwise
+        error('Invalid parser type selected, please choose one of: rangen | protrack | xlib \n');
+end
 
 browse_dir = fullfile(directory, extension_filter); % look for all files with the extension in given subfolder
 files = dir(browse_dir);
-out_dir = 'output'; % default output subfolder
 
-% map TP like 1=NTP, 2=CTP, 3=DTP as human readable simulation types
-TP = ["NTP", "CTP", "DTP"];
+[parent_folder_path] = fileparts(browse_dir);
+[current_folder,~] = fileparts(parent_folder_path);
+[~, child_folder] = fileparts(parent_folder_path);
+
+out_dir = strcat(current_folder,'/',child_folder,'_output','/'); % same level as input directory
 
 if exist(directory, 'dir')
-   mkdir(directory, out_dir);
+   mkdir(out_dir);
 else
    warning('%s directory does not exist!\n', directory);
    quit cancel;
 end
 
+% map TP like 1=NTP, 2=CTP, 3=DTP as human readable simulation types
+TP = ["NTP", "CTP", "DTP"];
 
 for i=1:size(files,1) % iterate through all files in given directory
     
@@ -39,11 +58,11 @@ for i=1:size(files,1) % iterate through all files in given directory
     [~,filename,~] = fileparts(fullfile(files(i,1).folder,files(i,1).name)); % get filename without extension
     
     [PDM, constr, num_r_resources, num_modes, num_activities, sim_type] = parser(fullfile(files(i,1).folder,files(i,1).name),j); % parse all files for all simulation types
-    save(strcat(fullfile(files(i,1).folder,out_dir,filename),'_',TP(1,j)),'PDM','constr','num_r_resources','num_modes','num_activities','sim_type'); % save each variable "PDM" to the <folder name>/output folder
+    save(strcat(out_dir,filename,'_',TP(1,j)),'PDM','constr','num_r_resources','num_modes','num_activities','sim_type'); % save each variable "PDM" to the <folder name>/output folder
     
-    if (j == 1) % save DSM only once per file
+    if (j == 1) % save DSM only once per file for debug
         DSM = parser(fullfile(files(i,1).folder,files(i,1).name),0); % parse only DSM
-        save(strcat(fullfile(files(i,1).folder,out_dir,filename),'_DSM'),'DSM');
+        save(strcat(out_dir,filename,'_DSM'),'DSM');
     end
     
     end % simulation types
