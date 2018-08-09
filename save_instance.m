683967eb47b7f5f2e75a63887d0a22e463fc6b43
novakge <38750734+novakge@users.noreply.github.com>
2018-08-09 14:48:00 +0200

    folder structure changed; rangen renamed

--- save_instance.m
+++ save_instance.m
@@ -11,7 +11,7 @@ function save_instance(directory, parser_type)
 switch parser_type
     
     case 'rangen'
-        if exist('parse_instance') % RanGen1-2
+        if exist('parse_rangen') % RanGen1-2
             parser = @parse_rangen; % create common function handle
             extension_filter = '*.rcp'; % select extension
         end
@@ -58,8 +58,13 @@ for i=1:size(files,1) % iterate through all files in given directory
     [~,filename,~] = fileparts(fullfile(files(i,1).folder,files(i,1).name)); % get filename without extension
     
     [PDM, constr, num_r_resources, num_modes, num_activities, sim_type] = parser(fullfile(files(i,1).folder,files(i,1).name),j); % parse all files for all simulation types
-    save(strcat(out_dir,filename,'_',TP(1,j)),'PDM','constr','num_r_resources','num_modes','num_activities','sim_type'); % save each variable "PDM" to the <folder name>/output folder
     
+    if sim_type > 0 % if the given simulation type is supported for the dataset
+        save(strcat(out_dir,filename,'_',TP(1,j)),'PDM','constr','num_r_resources','num_modes','num_activities','sim_type'); % save each variable "PDM" to the <folder name>/output folder
+    else
+        % skip the given trade-off for this instance (not supported)
+    end
+        
     if (j == 1) % save DSM only once per file for debug
         DSM = parser(fullfile(files(i,1).folder,files(i,1).name),0); % parse only DSM
         save(strcat(out_dir,filename,'_DSM'),'DSM');
