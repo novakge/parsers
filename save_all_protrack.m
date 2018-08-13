463c9882db8ae858e6bf7b3a4aa0d5e0b3ceeb58
novakge <38750734+novakge@users.noreply.github.com>
2018-08-13 13:58:19 +0200

    add batch for protrack export

---
+++ save_all_protrack.m
@@ -0,0 +1,13 @@
+% batch processing of predefined *.p2x files to (parsed) *.mat files as an input for simulation
+% example: save_all_protrack
+function save_all_protrack
+tic;
+fprintf('in progress...');
+save_instance('data/protrack', 'protrack')
+fprintf('...');
+fprintf('still working...');
+fprintf('done!\n');
+toc;
+
+
+% any additional dataset goes here
