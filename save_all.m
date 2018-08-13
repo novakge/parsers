8f399daf7b1575fbe1b726aa0b4aec31cd11c580
novakge <38750734+novakge@users.noreply.github.com>
2018-08-13 13:59:23 +0200

    add batch for exporting all datasets (calling dedicated script for each parser package)

---
+++ save_all.m
@@ -0,0 +1,16 @@
+% batch processing of predefined *.mm files to (parsed) *.mat files as an input for simulation
+% example: save_all_instance
+function save_all_instance
+tic;
+fprintf('in progress...');
+fprintf('saving rangen dataset...');
+save_all_rangen
+fprintf('saving protrack dataset...');
+save_all_protrack
+fprintf('saving psplib/mmlib datasets...');
+save_all_xlib
+fprintf('all done!\n');
+toc;
+
+
+% any additional dataset goes here
