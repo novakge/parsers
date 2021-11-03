e80d04c8d40b3631abb69423defa9d96fad48c96
novakge <jsmeqd@gmail.com>
2021-11-03 13:58:28 +0100

    add libraries to batch parse

--- save_all_rcmp.m
+++ save_all_rcmp.m
@@ -5,12 +5,24 @@ tic;
 fprintf('in progress...');
 save_instance('data/BY', 'rcmp')
 fprintf('...');
-save_instance('data/MPLIB1', 'rcmp')
+save_instance('data/RCMPSPLIB', 'rcmp')
+
+save_instance('data/MPLIB1/MPLIB 1 - Set 1', 'rcmp')
 fprintf('...');
-save_instance('data/MPLIB2', 'rcmp')
+save_instance('data/MPLIB1/MPLIB 1 - Set 2', 'rcmp')
+fprintf('...');
+save_instance('data/MPLIB1/MPLIB 1 - Set 3', 'rcmp')
 fprintf('...');
 fprintf('still working...');
-save_instance('data/RCMPSPLIB', 'rcmp')
+save_instance('data/MPLIB2/MPLIB 2 - Set 1', 'rcmp')
+fprintf('...');
+save_instance('data/MPLIB2/MPLIB 2 - Set 2', 'rcmp')
+fprintf('...');
+save_instance('data/MPLIB2/MPLIB 2 - Set 3', 'rcmp')
+fprintf('...');
+save_instance('data/MPLIB2/MPLIB 2 - Set 4', 'rcmp')
+fprintf('...');
+
 fprintf('done!\n');
 toc;
 
