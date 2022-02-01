e6ce08351f3192b7ff37193a69da6ba005528586
novakge <jsmeqd@gmail.com>
2022-02-01 13:40:26 +0100

    adaptation for superset output

--- parse_rcmp_test.m
+++ parse_rcmp_test.m
@@ -22,68 +22,39 @@ testCase.TestData.RCMP_BY_OWN_002 = 'test_data/test_001.rcmp';
 end
 
 function test_RCMP_BY_001(testCase)
-DSM_cell = parse_rcmp(testCase.TestData.RCMP_BY_001,0);
-DSM_mat = cell2mat(DSM_cell(1));
-[i1,i2,i3,i4,i5,i6] = indicators(DSM_mat); % 1st project's DSM
-actSolution = [i1,i2];
-expSolution = [20,0.11];
+DSM = parse_rcmp(testCase.TestData.RCMP_BY_001,0);
+[i1,i2,i3,i4,i5,i6] = indicators(DSM); % global DSM
+actSolution = [i1,i2,i3,i4,i5,i6];
+expSolution = [60,0.05,0.547,0.0053,0.722,0.7262];
 verifyEqual(testCase,actSolution,expSolution,'AbsTol',0.01)
 end
 
 function test_RCMP_BY_002(testCase)
-DSM_cell = parse_rcmp(testCase.TestData.RCMP_BY_002,0);
-DSM_mat = cell2mat(DSM_cell(2));
-[i1,i2,i3,i4,i5,i6] = indicators(DSM_mat); % 2nd project's DSM
-actSolution = [i1,i2];
-expSolution = [20,0.16];
-verifyEqual(testCase,actSolution,expSolution,'AbsTol',0.01)
-end
-
-function test_RCMP_BY_003(testCase)
-DSM_cell = parse_rcmp(testCase.TestData.RCMP_BY_003,0);
-DSM_mat = cell2mat(DSM_cell(3));
-[i1,i2,i3,i4,i5,i6] = indicators(DSM_mat); % 3rd project's DSM
-actSolution = [i1,i2];
-expSolution = [20,0.05];
-verifyEqual(testCase,actSolution,expSolution,'AbsTol',0.01)
-end
-
-function test_RCMP_BY_004(testCase)
-DSM_cell = parse_rcmp(testCase.TestData.RCMP_BY_004,0);
-DSM_mat = cell2mat(DSM_cell(1));
-[i1,i2,i3,i4,i5,i6] = indicators(DSM_mat); % 1st project's DSM
+[DSM,~,~,~,~,num_activities,~,~] = parse_rcmp(testCase.TestData.RCMP_BY_002,0);
+DSM = DSM(1:num_activities(1),1:num_activities(1)); % select first project's DSM
+[i1,i2,i3,i4,i5,i6] = indicators(DSM);
 actSolution = [i1,i2,i3,i4,i5,i6];
-expSolution = [120,0.109,0.335,0.008,0.825,0.202];
-verifyEqual(testCase,actSolution,expSolution,'AbsTol',0.01)
-end
-
-function test_RCMP_BY_005(testCase)
-DSM_cell = parse_rcmp(testCase.TestData.RCMP_BY_005,0);
-DSM_mat = cell2mat(DSM_cell(20));
-[i1,i2,i3,i4,i5,i6] = indicators(DSM_mat); % 20th project's DSM
-actSolution = [i1,i2,i3,i4,i5,i6];
-expSolution = [120,0.126,0.251,0.012,0.805,0.167];
+expSolution = [20,0.052,0.444,0,1,0.611];
 verifyEqual(testCase,actSolution,expSolution,'AbsTol',0.01)
 end
 
 function test_RCMP_BY_OWN_001(testCase)
-PDM = parse_rcmp(testCase.TestData.RCMP_BY_OWN_001,0); % DSM
-DSM_selected = cell2mat(PDM(4));
-[i1,i2,i3,i4,i5,i6] = indicators(DSM_selected); % 4th project's PDM
+[DSM,~,~,~,~,num_activities,~,~] = parse_rcmp(testCase.TestData.RCMP_BY_OWN_001,0); % DSM
+DSM = DSM(sum(num_activities(1:end-1))+1:sum(num_activities(1:end)),sum(num_activities(1:end-1))+1:sum(num_activities(1:end)));
+[i1,i2,i3,i4,i5,i6] = indicators(DSM); % last project's PDM
 actSolution = [i1,i2,i3,i4,i5,i6];
 expSolution = [11,0.2,0.5,0.0833,0.333,0.313];
 verifyEqual(testCase,actSolution,expSolution,'AbsTol',0.01)
 end
 
 function test_RCMP_BY_OWN_002(testCase)
-[PDM,~,num_r_resources,~,~,num_activities,~] = parse_rcmp(testCase.TestData.RCMP_BY_OWN_002,3); % DTP
-PDM_sel = cell2mat(PDM(2)); % select the 2nd project's PDM
-n = size(PDM_sel,1);
-TD_act = PDM_sel(:,n+1);
-CD_act = PDM_sel(:,n+2);
-RD_act = PDM_sel(:,n+3:n+3+num_r_resources-1);
+[PDM,~,num_r_resources,~,~,num_activities,~,~] = parse_rcmp(testCase.TestData.RCMP_BY_OWN_002,3); % DTP
+n = size(PDM,1);
+TD_act = PDM(:,n+1);
+CD_act = PDM(:,n+2);
+RD_act = PDM(:,n+3:n+3+num_r_resources-1);
 actSolution = [TD_act,CD_act,RD_act];
-expSolution = [[2;3;4;5;6;7;8;9;10;11;12],[0;0;0;0;0;0;0;0;0;0;0],[11,22,33;55,66,77;99,100,101;103,104,105;107,108,109;0,0,0;1,1,1;2,2,2;3,3,3;4,4,4;5,5,5]];
+expSolution = [[2;3;4;5;6;7;8;9;10;11;12;2;3;4;5;6;7;8;9;10;11;12;2;3;4;5;6;7;8;9;10;11;12;2;3;4;5;6;7;8;9;10;11;12],[0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0],[11,22,33;55,66,77;99,100,101;103,104,105;107,108,109;0,0,0;1,1,1;2,2,2;3,3,3;4,4,4;5,5,5;11,22,33;55,66,77;99,100,101;103,104,105;107,108,109;0,0,0;1,1,1;2,2,2;3,3,3;4,4,4;5,5,5;11,22,33;55,66,77;99,100,101;103,104,105;107,108,109;0,0,0;1,1,1;2,2,2;3,3,3;4,4,4;5,5,5;11,22,33;55,66,77;99,100,101;103,104,105;107,108,109;0,0,0;1,1,1;2,2,2;3,3,3;4,4,4;5,5,5]];
 verifyEqual(testCase,actSolution,expSolution,'AbsTol',0.01)
 end
 
